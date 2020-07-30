6b0ef2376fbe4ef2609b7877d65c3131709ddb75
Vladimir <ecofovka@gmail.com>
2020-07-30 13:24:36 +0200

    Created HTML structure and styles

---
+++ app.js
@@ -0,0 +1,8 @@
+connst app = () => {
+   const song = document.querySelector('.song');
+   const play = document.querySelector('.play');
+   const outline = document.querySelector('.moving-outline circle');
+}
+
+
+app();
